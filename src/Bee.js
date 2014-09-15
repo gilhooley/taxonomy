892a770d509064aaf01da2b386bfa6d07c2a2b66
KAG <k.gilhooley@gmail.com>
2014-09-15 12:43:30 -0700

    YOU get bees and YOU get bees and

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
-var Bee = function() {
+
+var Bee = function(age) {
+  Grub.call(this, age);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "Keep on growing";
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,15 @@
-var HoneyMakerBee = function() {
+var HoneyMakerBee = function(age) {
+  Bee.call(this, age);
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
+
+
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function () { this.honeyPot++; }
+HoneyMakerBee.prototype.giveHoney = function () { this.honeyPot--; }
+
 
